f391f676e65a804df20aa7c41f58d9ea5298db5b
Michael Rans <rans@email.com>
2016-06-21 16:02:20 +0200

    Sort

--- js/site.js
+++ js/site.js
@@ -381,7 +381,6 @@ function drawGraph(data,percent){
 		}
 		return parseInt(a) - parseInt(b);
 	}
-	//data.sort(sortItems);
 	x.domain(data.map(function(d) {return d.key; }));
 
 	var maxy = d3.max(data,function(d){
@@ -409,6 +408,7 @@ function drawGraph(data,percent){
 
 	svg.append("g").selectAll("rect")
 	    .data(data)
+			.sort(sortItems)
 			.enter()
 	    .append("rect")
 	    .attr("x", function(d,i) { return x(d.key)+3; })
